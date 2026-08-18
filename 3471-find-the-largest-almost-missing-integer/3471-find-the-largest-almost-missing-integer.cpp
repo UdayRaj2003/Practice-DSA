04dbc3d38388e6d6250bbd0a35d4ee72c717c7f5
Uday Raj Gupta <156777736+UdayRaj2003@users.noreply.github.com>
2026-08-18 15:23:49 +0530

    Time: 0 ms (100%), Space: 29 MB (78.95%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -3,50 +3,36 @@ public:
     int largestInteger(vector<int>& nums, int k) {
         int n = nums.size();
 
+        vector<int> freq(51, 0);
+
+        for (int x : nums) {
+            freq[x]++;
+        }
+        if (k == n) {
+            return *max_element(nums.begin(), nums.end());
+        }
         if (k == 1) {
-            unordered_map<int, int> freq;
-
-            for (int x : nums)
-                freq[x]++;
-
             int ans = -1;
 
-            for (auto &[x, f] : freq) {
-                if (f == 1)
+            for (int x : nums) {
+                if (freq[x] == 1) {
                     ans = max(ans, x);
+                }
             }
 
             return ans;
         }
 
-        // Case 2: k == n
-        if (k == n) {
-            return *max_element(nums.begin(), nums.end());
+        int ans = -1;
+
+        if (freq[nums[0]] == 1) {
+            ans = max(ans, nums[0]);
         }
 
-        // Case 3: 1 < k < n
-        int first = nums[0];
-        int last = nums[n - 1];
-
-        int cf = 0, cl = 0;
-
-        for (int x : nums) {
-            if (x == first) cf++;
-            if (x == last) cl++;
+        if (freq[nums[n - 1]] == 1) {
+            ans = max(ans, nums[n - 1]);
         }
 
-        bool firstUnique = (cf == 1);
-        bool lastUnique = (cl == 1);
-
-        if (firstUnique && lastUnique)
-            return max(first, last);
-
-        if (firstUnique)
-            return first;
-
-        if (lastUnique)
-            return last;
-
-        return -1;
+        return ans;
     }
 };
